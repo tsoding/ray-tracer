d169ef88cdc1a0b28ef60f70ef561fc663dbf1ba
rexim <reximkut@gmail.com>
2018-08-22 05:30:14 +0700

    (#57) Inline plane.hpp into wall.hpp

--- src/wall.hpp
+++ src/wall.hpp
@@ -1,8 +1,9 @@
 #ifndef WALL_H_
 #define WALL_H_
 
-#include "./plane.hpp"
 #include "./color.hpp"
+
+using plane = vec<float, 4>;
 
 struct Wall {
     plane p;
@@ -11,6 +12,10 @@ struct Wall {
 
 inline std::ostream& operator<<(std::ostream& os, const Wall&wall) {
     return os << "Wall{ p" << wall.p << " c" << wall.c << "}";
+}
+
+inline float dot(const plane &p, const vec3<float> &v) {
+    return p.v[0] * v.v[0] + p.v[1] * v.v[1] + p.v[2] * v.v[2] + p.v[3];
 }
 
 #endif  // WALL_H_
