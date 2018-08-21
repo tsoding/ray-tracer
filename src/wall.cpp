5b167c25de715efef44d8ffce08df4012f0105fc
rexim <reximkut@gmail.com>
2018-08-22 05:43:45 +0700

    (#57) const ref

--- src/wall.cpp
+++ src/wall.cpp
@@ -8,6 +8,6 @@ static float dot(const plane &p, const vec3<float> &v) {
     return p.v[0] * v.v[0] + p.v[1] * v.v[1] + p.v[2] * v.v[2] + p.v[3];
 }
 
-bool is_ray_behind_wall(const Wall &wall, vec3<float> ray) {
+bool is_ray_behind_wall(const Wall &wall, const vec3<float> &ray) {
     return dot(wall.p, ray) < 0.0f;
 }
--- src/wall.hpp
+++ src/wall.hpp
@@ -11,6 +11,6 @@ struct Wall {
 };
 
 std::ostream& operator<<(std::ostream& os, const Wall&wall);
-bool is_ray_behind_wall(const Wall &wall, vec3<float> ray);
+bool is_ray_behind_wall(const Wall &wall, const vec3<float> &ray);
 
 #endif  // WALL_H_
