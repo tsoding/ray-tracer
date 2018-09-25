37ef265acc7bdb70268f092fd7b11f4b836d0f4a
rexim <reximkut@gmail.com>
2018-09-26 00:12:13 +0700

    (#77) Implement trace for walls

--- src/scene.cpp
+++ src/scene.cpp
@@ -97,7 +97,47 @@ color march(float x, float y,
     return {0.0f, 0.0f, 0.0f};
 }
 
-color trace(float x, float y, const Scene &scene, vec3<float> dir) {
-    // TODO(#77): trace() function is not implemented
-    return color{0.0f, 0.0f, 0.0f};
+// TODO: solve_plane_line is not a very descriptive name
+float solve_plane_line(const plane &p,
+                       const v3f &line_start,
+                       const v3f &line_direction) {
+    const float A = p.v[0], B = p.v[1], C = p.v[2], D = p.v[3];
+    const float x1 = line_start.v[0], y1 = line_start.v[1], z1 = line_start.v[2];
+    const float dx = line_direction.v[0], dy = line_direction.v[1], dz = line_direction.v[2];
+    // TODO: GachiPls division by zero
+    const float t = - (A * x1 + B * y1 + C * z1 + D) / (A * dx + B * dy + C * dz);
+    return t;
+}
+
+color trace(float x, float y, const Scene &scene, const vec3<float> &dir) {
+    const vec3<float> ray = {x, y, 0.0f};
+
+    bool found = false;
+    vec3<float> closest_point;
+    color closest_color;
+    size_t id = 0;
+
+    for (size_t i = 0; i < scene.walls.size(); ++i) {
+        const float t = solve_plane_line(scene.walls[i].p, ray, dir);
+
+        if (t < 0.0f) {
+            continue;
+        }
+
+        if (!found) {
+            found = true;
+            closest_point = ray + t * dir;
+            closest_color = scene.walls[i].c;
+            id = i;
+        } else {
+            const vec3<float> new_closest_point = ray + t * dir;
+            if (len(ray - new_closest_point) < len(ray - closest_point)) {
+                closest_point = new_closest_point;
+                closest_color = scene.walls[i].c;
+                id = i;
+            }
+        }
+    }
+
+    return closest_color;
 }
--- src/scene.hpp
+++ src/scene.hpp
@@ -18,6 +18,8 @@ struct Scene {
 std::ostream& operator<<(std::ostream& os, const Scene&scene);
 const Scene load_scene_from_file(const std::string &filename);
 color march(float x, float y, const Scene &scene, vec3<float> dir);
-color trace(float x, float y, const Scene &scene, vec3<float> dir);
+color trace(float x, float y,
+            const Scene &scene,
+            const vec3<float> &dir);
 
 #endif  // SCENE_HPP_
