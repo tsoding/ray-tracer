8a8dbb7b9a542bcba9fc8d7b66cf2c5468a5b57f
rexim <reximkut@gmail.com>
2018-10-17 00:10:54 +0700

    (#96) Introduce ray

---
+++ src/ray.cpp
@@ -0,0 +1,13 @@
+#include "ray.hpp"
+
+Ray collide_ray_with_sphere(const Ray &ray, const Sphere &sphere)
+{
+    // TODO: collide_ray_with_sphere is not implemented
+    return ray;
+}
+
+Ray collide_ray_with_wall(const Ray &ray, const Wall &wall)
+{
+    // TODO: collide_ray_with_wall is not implemented
+    return ray;
+}
