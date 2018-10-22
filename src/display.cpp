14ff0087ef0d3234b3af9f878a7977c27fdf518f
rexim <reximkut@gmail.com>
2018-10-23 04:57:20 +0700

    Remove OOP madness

--- src/display.cpp
+++ src/display.cpp
@@ -5,57 +5,52 @@
 #include "display.hpp"
 
 Display::Display(size_t width, size_t height):
-    m_width(width),
-    m_height(height),
-    m_data(new Color[width * height]) {
+    width(width),
+    height(height),
+    data(new Color[width * height]) {
 }
 
 Display::Display(const Display &display):
-    m_width(display.m_width),
-    m_height(display.m_height),
-    m_data(new Color[m_width * m_height]) {
-    std::copy(display.m_data,
-              display.m_data + m_width * m_height,
-              m_data);
+    width(display.width),
+    height(display.height),
+    data(new Color[width * height]) {
+    std::copy(display.data,
+              display.data + width * height,
+              data);
 }
 
 Display::Display(Display &&display):
-    m_width(display.m_width),
-    m_height(display.m_height),
-    m_data(display.m_data) {
+    width(display.width),
+    height(display.height),
+    data(display.data) {
 }
 
 Display::~Display() {
-    delete[] m_data;
-}
-
-size_t Display::width() const {
-    return m_width;
-}
-
-size_t Display::height() const {
-    return m_height;
+    delete[] data;
 }
 
 void Display::put(size_t row, size_t col, const Color &c) {
-    m_data[row * m_width + col] = c;
+    data[row * width + col] = c;
 }
 
 Color Display::pixel(size_t row, size_t col) const {
-    return m_data[row * m_width + col];
+    return data[row * width + col];
 }
 
-void Display::save_as_ppm(const std::string &filepath) const {
-    std::ofstream fout(filepath, std::ofstream::out | std::ostream::binary);
+void save_as_ppm(const Display &display,
+                 const std::string &filepath) {
+    std::ofstream fout(
+        filepath,
+        std::ofstream::out | std::ostream::binary);
 
     fout << "P6" << std::endl;
-    fout << m_width << " " << m_height << std::endl;
+    fout << display.width << " " << display.height << std::endl;
     fout << 255 << std::endl;
 
-    for (size_t row = 0; row < m_height; ++row) {
-        for (size_t col = 0; col < m_width; ++col) {
+    for (size_t row = 0; row < display.height; ++row) {
+        for (size_t col = 0; col < display.width; ++col) {
             for (size_t k = 0; k < 3; ++k) {
-                const float x = 255.0f * m_data[row * m_width + col].v[k];
+                const float x = 255.0f * display.pixel(row, col).v[k];
                 fout.put(static_cast<char>(x));
             }
         }
--- src/ray.hpp
+++ src/ray.hpp
@@ -13,6 +13,10 @@ struct Ray {
     bool absorbed;
 };
 
+inline Ray ray(const v3f &origin, const v3f &dir, const Color &color) {
+    return {origin, dir, color, false};
+}
+
 Ray void_ray(const Ray &ray);
 Ray collide_ray_with_sphere(const Ray &ray, const Sphere &sphere);
 Ray collide_ray_with_wall(const Ray &ray, const Wall &wall);
